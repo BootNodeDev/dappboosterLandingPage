e449279e373f2394375320c2a40a2ee1e8fd569d
Gabito Esmiapodo <4015436+gabitoesmiapodo@users.noreply.github.com>
2025-04-24 11:47:03 -0300

    chore: add vercel support

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,7 +1,14 @@
 // @ts-check
 import { defineConfig } from "astro/config";
+// Import /static for a static site
+import vercelStatic from "@astrojs/vercel/static";
 
 // https://astro.build/config
 export default defineConfig({
-  output: "server",
+  output: "static",
+  adapter: vercelStatic({
+    webAnalytics: {
+      enabled: true,
+    },
+  }),
 });
